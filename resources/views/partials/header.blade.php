7dd80c274ed15620758bb9997a752ab6b4f3102e
MGF2 <fioremartina@hotmail.com>
2020-10-08 17:10:03 +0200

    Done laravel first steps

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+  <img src="https://cdn.shortpixel.ai/client/q_lossy,ret_img/https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="molisana-logo">
+  <nav>
+    <ul>
+      <li><a href="{{ route('home') }}">Home</a></li>
+      <li><a href="{{ route('prodotti') }}">Prodotti</a></li>
+      <li><a href="{{ route('contatti') }}">Contatti</a></li>
+    </ul>
+  </nav>
+</header>
