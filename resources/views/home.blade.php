df4ef587db4ef431924a1fb959e883166b195c0a
MGF2 <fioremartina@hotmail.com>
2020-10-09 18:36:50 +0200

    still

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,3 +2,10 @@
     @extends('layout.app')
     @section('titolo','La Molisana Home')
     @section('home','active')
+    @section('main')
+      <div class="ban">
+        <img src="https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_700,h_180/https://www.lamolisana.it/wp-content/uploads/2020/09/header-concorso-1.jpg" alt="banner">
+        <div class="vp-preview" data-thumb-width="1400" style="background-image: url('https://i.vimeocdn.com/video/915001114.webp?mw=1400&amp;mh=788');">
+        </div>
+      </div>
+    @endsection
