78881a049e1f1e19c9d7177fd70b066d3d5f12d7
MGF2 <fioremartina@hotmail.com>
2020-10-12 18:30:38 +0200

    Controller

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,21 +1,3 @@
-@php
-$cards = config('prodotti');
-
-$lunghe = [];
-$corte = [];
-$cortissime = [];
-
-foreach ($cards as $card) {
-  if ( $card['tipo'] == 'lunga') {
-    $lunghe[] = $card;
-  } elseif ($card['tipo'] == 'corta') {
-    $corte[] = $card;
-  } elseif ($card['tipo'] == 'cortissima') {
-    $cortissime[] = $card;
-  }
-}
-// dd($cards,$lunghe,$corte,$cortissime);
-@endphp
 
     @extends('layout.app')
     @section('titolo','La Molisana Prodotti')
