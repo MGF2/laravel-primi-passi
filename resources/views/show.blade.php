091a75114758793c0e4670d9eda406f0ccc28fd6
MGF2 <fioremartina@hotmail.com>
2020-10-13 17:42:40 +0200

    style fixed

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,6 +7,9 @@
           <div class="card">
             <h3>{{ $card['titolo'] }}</h3>
             <img src="{{ $card['src']}}" alt="{{ $card['titolo'] }}">
+            <p class="card-text">{{ $card['descrizione']}}</p>
+            <p class="card-text">Cottura: {{ $card['cottura']}}</p>
+            <p class="card-text">Peso:{{ $card['peso']}}</p>
           </div>
       </div>
       @endsection
